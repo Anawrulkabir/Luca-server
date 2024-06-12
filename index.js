aeba209b1886f12664172d2ca396f4866d792c85
Anawrulkabir <mdanawrulkabirfahad123@gmail.com>
2024-06-12 21:48:01 +0600

    CORS policy link updated

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ const port = process.env.PORT || 8000
 // const resend = new Resend(api_key)
 // middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'https://user-email-password-auth-8dfb6.web.app',
+  ],
   credentials: true,
   optionSuccessStatus: 200,
 }
